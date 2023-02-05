3f4536e924e17b65e5cd804d2fdfe21dc276ca7b
aSegaN <asegandiaye@icloud.com>
2023-02-05 07:37:40 +0000

    Initial commit

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Pizza
 
@@ -5,7 +6,12 @@ from .models import Pizza
 # Create your views here.
 # /menu
 def index(request):
+    """
     pizzas = Pizza.objects.all()
-    pizzas_names = [pizza.nom for pizza in pizzas]
-    pizzas_names_str = ", ".join(pizzas_names)
-    return HttpResponse(pizzas_names_str)
+    pizzas_names_and_prices = [pizza.nom + ":" + str(pizza.prix) + "$" for pizza in pizzas]
+    pizzas_names_str_and_prices = ", ".join(pizzas_names_and_prices)
+    return HttpResponse(pizzas_names_str_and_prices)
+    """
+    return render(request, 'menu/index.html')
+
+
